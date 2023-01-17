3b0c08ad6e43c40104595ecc0c3c0bb6cf44d9d2
TomekWebDev <tommaso.mangiagalli@gmail.com>
2023-01-17 16:26:57 +0100

    done

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,26 +1,35 @@
 @extends('layouts.app')
 
 @section('main')
-    <table class="table table-dark">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-            </tr>
-        </thead>
-        <tbody>
+    <h1 class="text-center my-4">Meglio di trenitalia</h1>
 
-            @foreach ($trains as $item)
+    <div class=" d-flex justify-content-center">
+
+        <table class="table table-dark text-center">
+            <thead class="text-danger">
                 <tr>
-                    <th scope="row">1</th>
-                    <td>{{ $item['destinazione'] }}</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-            @endforeach
 
-        </tbody>
-    </table>
+                    <th scope="col">Destinazione</th>
+                    <th scope="col">Partenza</th>
+                    <th scope="col">Arrivo</th>
+                    <th scope="col">Binario</th>
+                    <th scope="col">Compagnia</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                @foreach ($trains as $item)
+                    <tr>
+
+                        <td>{{ $item->destinazione }}</td>
+                        <td>{{ $item->partenza }}</td>
+                        <td>{{ $item->arrivo }}</td>
+                        <td>{{ $item->binario }}</td>
+                        <td>{{ $item->compagnia }}</td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    </div>
 @endsection
